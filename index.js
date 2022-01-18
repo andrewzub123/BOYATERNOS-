86caecf863b94ded8bb4cfd8893fb59ad6bf0a97
andrewzub123 <96284254+andrewzub123@users.noreply.github.com>
2022-01-18 13:54:25 +0930

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var moveinterval = 2; // 2 second movement interval
 var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
 var host = data["ip"];
 var username = data["name"]
-var nightskip = data["night-skip"]
+var nightskip = data["auto-night-skip"]
 var bot = mineflayer.createBot({
   host: host,
   username: username
